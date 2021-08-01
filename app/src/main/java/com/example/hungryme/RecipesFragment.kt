8a618dbe3bddfe3023ecd8b23bdbf8852e36882d
Aditya <rameshaditya2001@gmail.com>
2021-08-01 23:28:09 +0530

    Model Classes Created For API

--- app/src/main/java/com/example/hungryme/RecipesFragment.kt
+++ app/src/main/java/com/example/hungryme/RecipesFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.todkars.shimmer.ShimmerRecyclerView
 
 
 class RecipesFragment : Fragment() {
@@ -15,7 +16,10 @@ class RecipesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
+        val view = inflater.inflate(R.layout.fragment_recipes, container, false)
+        val recyclerViewShimmer: ShimmerRecyclerView = view.findViewById(R.id.shimmer_recycler_view)
+        recyclerViewShimmer.showShimmer()
+        return view
     }
 
 }
