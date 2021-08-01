f973ed999e1d964f44c529ab7b0060df447e3ee4
Aditya <rameshaditya2001@gmail.com>
2021-08-01 18:17:25 +0530

    Dependencies Installed

--- app/src/main/java/com/example/hungryme/MainActivity.kt
+++ app/src/main/java/com/example/hungryme/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.hungryme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        actionBar?.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.action_bar_gradient))
+        supportActionBar?.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.action_bar_gradient))
         setContentView(R.layout.activity_main)
     }
+
 }
