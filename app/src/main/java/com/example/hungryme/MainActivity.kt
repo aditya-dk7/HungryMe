18a83d84f98dcc479008f79aaa00232eb56949a9
Aditya <rameshaditya2001@gmail.com>
2021-08-01 19:43:05 +0530

    Bottom Navigation and Fragments Added.

--- app/src/main/java/com/example/hungryme/MainActivity.kt
+++ app/src/main/java/com/example/hungryme/MainActivity.kt
@@ -3,13 +3,47 @@ package com.example.hungryme
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.content.ContextCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Changed the action bar to a suitable gradient.
         actionBar?.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.action_bar_gradient))
         supportActionBar?.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.action_bar_gradient))
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.navHostFragment)
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.recipesFragment,
+                R.id.favouriteFoodFragment,
+                R.id.foodJokeFragment
+            )
+        )
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.navHostFragment) as NavHostFragment?
+        NavigationUI.setupWithNavController(
+            bottomNavigationView,
+            navHostFragment!!.navController
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
 }
